1254875df0677cf472345d23f96e3f268a0bed66
nasiriqbal786 <mehr_nasir@hotmail.com>
2018-08-09 16:02:38 +1000

    Refactored Map Impl to abstract ID generation. Closes #23.

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -11,11 +11,6 @@ import com.springframework.sfgpetclinic.services.OwnerService;
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
 
 	@Override
-	public Owner save(Owner object) {
-		return super.save(object.getId(), object);
-	}
-
-	@Override
 	public Set<Owner> findAll() {
 		return super.findAll();
 	}
@@ -26,8 +21,8 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 	}
 
 	@Override
-	Owner save(Long id, Owner object) {
-		return super.save(id, object);
+	public Owner save( Owner object) {
+		return super.save(object);
 	}
 
 	@Override
