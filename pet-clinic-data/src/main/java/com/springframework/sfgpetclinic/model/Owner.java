6eeaf7c8f51de4d8c9c412668448bb64797dec26
Nasir Iqbal <mehr_nasir@hotmail.com>
2018-08-12 15:11:42 +1000

    Added Visit Entity, updated owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
@@ -1,6 +1,17 @@
 package com.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 @SuppressWarnings("serial")
 public class Owner extends Person {
 
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 }
