fed52ebe7c3a21dee12b9edb0e4ab2701b5200e1
Nasir Iqbal <mehr_nasir@hotmail.com>
2018-08-12 22:11:07 +1000

    Specialty entity and Vet Specialty association. closes #27

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Vet.java
@@ -1,6 +1,17 @@
 package com.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 @SuppressWarnings("serial")
 public class Vet extends Person {
 
+	private Set<Specialty> specialties;
+
+	public Set<Specialty> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Specialty> specialties) {
+		this.specialties = specialties;
+	}
 }
