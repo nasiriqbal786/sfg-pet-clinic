077aa4aee67592f2fdaa37501abe1a0700eb02e9
nasiriqbal786 <mehr_nasir@hotmail.com>
2018-08-17 17:32:42 +1000

    Owners JPA capabilities

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Person.java
@@ -1,10 +1,19 @@
 package com.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 @SuppressWarnings("serial")
+@MappedSuperclass
 public class Person extends BaseEntity {
 	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(name = "sex")
 	private String sex;
 	
 	public String getFirstName() {
