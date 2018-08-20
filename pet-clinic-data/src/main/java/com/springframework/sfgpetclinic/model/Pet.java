3a56acdafedfe21c5aa0271b0f0576ebd60cf0dc
nasiriqbal786 <mehr_nasir@hotmail.com>
2018-08-20 13:47:37 +1000

    jpa mapping for Visits entity. Closes #39

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package com.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @SuppressWarnings("serial")
@@ -26,6 +30,9 @@ public class Pet extends BaseEntity {
 	
 	@Column(name = "birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 	
 	public String getName() {
 		return name;
@@ -52,6 +59,14 @@ public class Pet extends BaseEntity {
 	}
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 	
 	
