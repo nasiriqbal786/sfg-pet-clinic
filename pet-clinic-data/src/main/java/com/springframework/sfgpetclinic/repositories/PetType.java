0598bf69426bdae22b36618dabdc53bad396ff9f
nasiriqbal786 <mehr_nasir@hotmail.com>
2018-08-20 15:41:48 +1000

    Add Spring data jpa repositories. Closes #40

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/repositories/PetType.java
@@ -0,0 +1,7 @@
+package com.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetType extends CrudRepository<PetType, Long> {
+
+}
